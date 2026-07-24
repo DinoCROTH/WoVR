dff4622f54a042dbb07652e16943ca8486c34e35
DinoCROTH <pleiadesds2012@gmail.com>
2026-07-24 19:22:11 +0200

    Fix: only snap facing to HMD on stick release transition, not every frame

--- dxProxyWoVR/game_extras.cpp
+++ dxProxyWoVR/game_extras.cpp
@@ -3073,15 +3073,15 @@ void RunControllerGame()
                         {
                             lxRunning = false;
                             moveForwardStop();
+                            hRotationStickOffset = 0;
+                            XMVECTOR releaseAngles;
+                            if ((cfg_hmdOnward & 1) == 1)
+                                releaseAngles = GetAngles(matHMDPos);
+                            else
+                                releaseAngles = GetAngles(matController[0]);
+                            gRotation = EnsureProperRadians(releaseAngles.vector4_f32[1]);
+                            gCamRotation = gRotation;
                         }
-                        hRotationStickOffset = 0;
-                        XMVECTOR releaseAngles;
-                        if ((cfg_hmdOnward & 1) == 1)
-                            releaseAngles = GetAngles(matHMDPos);
-                        else
-                            releaseAngles = GetAngles(matController[0]);
-                        gRotation = EnsureProperRadians(releaseAngles.vector4_f32[1]);
-                        gCamRotation = gRotation;
                     }
                 }
             }
